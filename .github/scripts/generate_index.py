d16842cf780bda973369e206c74ea82713494f25
xiaoqiclock <128918038+xiaoqiclock@users.noreply.github.com>
2025-09-24 18:14:05 +0800

    Update generate_index.py

--- .github/scripts/generate_index.py
+++ .github/scripts/generate_index.py
@@ -1,46 +1,121 @@
-Q='index.html'
-import os,sys,unicodedata as D
-from pathlib import Path as A
-from datetime import datetime as Z
-a={'.git','.github',Q,'.nojekyll'}
-def C(text):
-	A=0
-	for B in text:
-		if D.east_asian_width(B)in('F','W'):A+=2
-		else:A+=1
-	return A
-def b(name,max_width):
-	F=max_width;D=name;J=C(D)
-	if J<=F:return D
-	E=F-3;A='';B=0;G=0
-	for(K,H)in enumerate(D):
-		I=C(H)
-		if B+I>E:break
-		A+=H;B+=I;G=K
-	if B+2>E:
-		while B+2>E and A:L=A[-1];A=A[:-1];B-=C(L);G-=1
-	return A+'..'+'>'
-def P(directory,base_dir):
-	Y='utf-8';X='original_name';W='display_width';V='size';U='path';T='/files/';O='is_dir';N='date';M='name';J=base_dir;I='/';D=directory;R=D/Q
-	if D==J:E=T
-	else:E=T+str(D.relative_to(J)).replace('\\',I)+I
-	B=[]
-	for A in D.iterdir():
-		if A.name in a:continue
-		c=Z.fromtimestamp(A.stat().st_mtime);d=c.strftime('%d-%b-%Y %H:%M');K=A.is_dir();H=A.name+I if K else A.name;e='-'if K else str(A.stat().st_size);B.append({M:H,U:H,N:d,V:e,O:K,W:C(H),X:H})
-	if not B:
-		F=f'''<html>
-<head><title>Index of {E}</title></head>
+import os
+import sys
+import unicodedata
+from pathlib import Path
+from datetime import datetime
+
+ignore_list = {'.git', '.github', 'index.html', '.nojekyll'}
+
+def get_east_asian_width_count(text):
+    count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in ('F', 'W'):
+            count += 2
+        else:
+            count += 1
+    return count
+
+def format_filename(name, max_width):
+    name_width = get_east_asian_width_count(name)
+    if name_width <= max_width:
+        return name
+    
+    available_width = max_width - 3
+    
+    truncated = ""
+    current_width = 0
+    last_valid_index = 0
+    
+    for i, char in enumerate(name):
+        char_width = get_east_asian_width_count(char)
+        if current_width + char_width > available_width:
+            break
+        truncated += char
+        current_width += char_width
+        last_valid_index = i
+    
+    if current_width + 2 > available_width:
+        while current_width + 2 > available_width and truncated:
+            last_char = truncated[-1]
+            truncated = truncated[:-1]
+            current_width -= get_east_asian_width_count(last_char)
+            last_valid_index -= 1
+    
+    return truncated + ".." + ">"
+
+def generate_index(directory, base_dir):
+    output_file = directory / "index.html"
+    
+    if directory == base_dir:
+        display_path = "/"
+    else:
+        display_path = "/" + str(directory.relative_to(base_dir)).replace("\\", "/") + "/"
+
+    items = []
+    for item in directory.iterdir():
+        if item.name in ignore_list:
+            continue
+            
+        mtime = datetime.fromtimestamp(item.stat().st_mtime)
+        date_str = mtime.strftime("%d-%b-%Y %H:%M")
+        
+        is_dir = item.is_dir()
+        name = item.name + "/" if is_dir else item.name
+        size = "-" if is_dir else str(item.stat().st_size)
+        
+        items.append({
+            'name': name,
+            'path': name,
+            'date': date_str,
+            'size': size,
+            'is_dir': is_dir,
+            'display_width': get_east_asian_width_count(name),
+            'original_name': name
+        })
+    
+    if not items:
+        html_content = f"""<html>
+<head><title>Index of {display_path}</title></head>
 <body>
-<h1>Index of {E}</h1><hr><pre><a href="../">../</a>
+<h1>Index of {display_path}</h1><hr><pre><a href="../">../</a>
 </pre><hr></body>
-</html>'''
-		with open(R,'w',encoding=Y)as L:L.write(F)
-		return
-	B.sort(key=lambda x:(not x[O],x[M].lower()));G=max(A[W]for A in B)if B else 0;G=min(G,50);F=f'<html>\n<head><title>Index of {E}</title></head>\n<body>\n<h1>Index of {E}</h1><hr><pre><a href="../">../</a>\n'
-	for A in B:S=b(A[X],G);f=C(S);g=' '*(G-f+50-G);h=' '*(20-len(A[N]));F+=f'<a href="{A[U]}">{S}</a>{g}{A[N]}{h}{A[V]}\n'
-	F+='</pre><hr></body>\n</html>'
-	with open(R,'w',encoding=Y)as L:L.write(F)
-	for A in B:
-		if A[O]:P(D/A[M].rstrip(I),J)
-if __name__=='__main__':B=A(sys.argv[1])if len(sys.argv)>1 else A('.');E=B;P(B,E)
+</html>"""
+        
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(html_content)
+        return
+
+    items.sort(key=lambda x: (not x['is_dir'], x['name'].lower()))
+
+    max_display_width = max(item['display_width'] for item in items) if items else 0
+    max_display_width = min(max_display_width, 50)
+    
+    html_content = f"""<html>
+<head><title>Index of {display_path}</title></head>
+<body>
+<h1>Index of {display_path}</h1><hr><pre><a href="../">../</a>
+"""
+
+    for item in items:
+        formatted_name = format_filename(item['original_name'], max_display_width)
+        
+        name_display_width = get_east_asian_width_count(formatted_name)
+        name_spacing = " " * (max_display_width - name_display_width + 50 - max_display_width)
+        date_spacing = " " * (20 - len(item['date']))
+        
+        html_content += f'<a href="{item["path"]}">{formatted_name}</a>{name_spacing}{item["date"]}{date_spacing}{item["size"]}\n'
+
+    html_content += """</pre><hr></body>
+</html>"""
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write(html_content)
+    
+    for item in items:
+        if item['is_dir']:
+            generate_index(directory / item['name'].rstrip('/'), base_dir)
+
+if __name__ == "__main__":
+    target_dir = Path(sys.argv[1]) if len(sys.argv) > 1 else Path('.')
+    base_dir = target_dir
+    generate_index(target_dir, base_dir)
